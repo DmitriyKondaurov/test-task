f055a1396f41ed40cbfcff96200cd2329c58ceab
Dmytro Kondaurov <invisibl@ukr.net>
2022-05-31 01:04:34 +0300

    add currency converter inputs

--- converter/src/app/components/currency-converter/currency-converter.component.ts
+++ converter/src/app/components/currency-converter/currency-converter.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ICurrency, ICurrencyItem} from "../../app-interfaces";
+import {RestApiService} from "../../services/rest-api.service";
 
 @Component({
   selector: 'app-currency-converter',
@@ -6,11 +8,24 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./currency-converter.component.css']
 })
 export class CurrencyConverterComponent implements OnInit {
-
-  constructor() { }
+  private url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+  euro: ICurrency | undefined;
+  dollar: ICurrency | undefined;
+  exchangeTo: ICurrencyItem = {
+    amount: 1,
+    currency: 980
+  };
+  exchangeFrom: ICurrencyItem = {
+    amount: 1,
+    currency: 840
+  };
+  constructor(private readonly restApiService: RestApiService) { }
 
   ngOnInit(): void {
-
+    this.restApiService.getCurrencyList(this.url).subscribe(currencyList => {
+      this.euro = currencyList.filter((item: ICurrency) => item.r030 === 978)[0];
+      this.dollar = currencyList.filter((item: ICurrency) => item.r030 === 840)[0];
+    })
   }
 
 }
