7684b9c9cb9912f8328826fb3f3956eb0fb12a61
Dmytro Kondaurov <invisibl@ukr.net>
2022-06-01 14:42:43 +0300

    simplify logic

--- converter/src/app/components/currency-converter/currency-converter.component.ts
+++ converter/src/app/components/currency-converter/currency-converter.component.ts
@@ -9,60 +9,42 @@ import {RestApiService} from "../../services/rest-api.service";
 })
 export class CurrencyConverterComponent implements OnInit {
   private url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
-  exchangeValue: ICurrency =
-    {
-      'r030': 980,
-      'txt': 'UAH',
-      'rate': 1,
-      'cc': 'string',
-      'exchangedate': new Date().toDateString(),
-    };
 
   exchangeTo: ICurrency = {
     'r030': 980,
-    'txt': 'гривня',
+    'txt': 'hryvna',
     'rate': 1,
     'cc': 'UAH',
     'exchangedate': new Date().toDateString(),
   }
 
   exchangeFrom: ICurrency = {
-    'r030': NaN,
-    'txt': '',
-    'rate': 0,
-    'cc': '',
+    'r030': 840,
+    'txt': 'dollar',
+    'rate': NaN,
+    'cc': 'USD',
     'exchangedate': new Date().toDateString(),
   }
 
   constructor(private readonly restApiService: RestApiService) { }
 
   ngOnInit(): void {
-    this.calcTo(978, 1);
+    this.calcTo(978, 1, true);
   }
 
-  calcTo(currencyCodeFrom: number, currencyCodeTo: number)  {
+  calcTo(currencyCodeFrom: number, currencyCodeTo: number, direction: boolean)  {
     this.restApiService.getCurrencyList(this.url).subscribe(currencyList => {
-      let currencyValue = currencyList.filter((item: ICurrency) => item.r030 === currencyCodeFrom)[0].rate;
-      this.exchangeFrom.rate = currencyValue * this.exchangeTo.rate;
-      this.exchangeFrom.r030 = currencyCodeFrom;
+      let currencyValueTo = this.getCurrencyValue(currencyCodeFrom, currencyList);
+      let currencyValueFrom = this.getCurrencyValue(currencyCodeTo, currencyList);
+      direction ?
+          this.exchangeFrom.rate = Math.round(this.exchangeTo.rate / currencyValueTo * currencyValueFrom * 100)/100
+        : this.exchangeTo.rate = Math.round(this.exchangeFrom.rate * currencyValueTo * currencyValueFrom * 100)/100
     })
   }
 
-  calcFrom()  {
-    this.restApiService.getCurrencyList(this.url).subscribe(currencyList => {
-      let currencyValueTo = 0;
-      let currencyValueFrom = 0;
-
-      if (currencyList.filter((item: ICurrency) => item.r030 == this.exchangeTo.r030).length > 0) {
-        currencyValueTo = currencyList.filter((item: ICurrency) => item.r030 == this.exchangeTo.r030)[0].rate;
-      } else currencyValueTo = 1
-
-      if (currencyList.filter((item: ICurrency) => item.r030 == this.exchangeFrom.r030).length > 0) {
-        currencyValueFrom = currencyList.filter((item: ICurrency) => item.r030 == this.exchangeFrom.r030)[0].rate;
-      } else currencyValueFrom = 1
-
-      this.exchangeTo.rate = Math.round(this.exchangeFrom.rate * currencyValueTo * currencyValueFrom * 100)/100;
-    })
+  getCurrencyValue(currencyCode: number, currencyList:ICurrency[]): number {
+    if (currencyList.filter((item: ICurrency) => item.r030 == currencyCode).length > 0) {
+      return currencyList.filter((item: ICurrency) => item.r030 == currencyCode)[0].rate;
+    } else return 1
   }
-
 }
